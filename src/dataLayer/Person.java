aee2077d2bc4623deb825c15b65d882c983efc3d
Karol Podlewski <karol.podlewski@hotmail.com>
2018-05-28 20:00:03 +0200

    Added constructor

--- src/dataLayer/Person.java
+++ src/dataLayer/Person.java
@@ -4,7 +4,14 @@ public class Person
 {
 	private String firstName;
 	private String lastName;
-
+	
+	public Person(String firstName, String lastName)
+	{
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
 	public String getFirstName()
 	{
 		return firstName;
