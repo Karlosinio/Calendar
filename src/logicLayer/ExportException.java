d2427327ee46a7a4283d71f5a9b28aa0918f22c3
Karol Podlewski <karol.podlewski@hotmail.com>
2018-05-28 18:46:06 +0200

    Renamed Saver etc to Exporter etc

---
+++ src/logicLayer/ExportException.java
@@ -0,0 +1,10 @@
+package logicLayer;
+
+@SuppressWarnings("serial")
+public class ExportException extends LogicLayerException
+{
+	public ExportException(String arg0)
+	{
+		super(arg0);
+	}
+}
--- src/logicLayer/Saver.java
+++
@@ -1,9 +0,0 @@
-package logicLayer;
-
-import dataLayer.DataService;
-
-
-public interface Saver
-{
-	public void save(String fileName, DataService data) throws SaveException;
-}
