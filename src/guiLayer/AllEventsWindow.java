1e5771a356f1c323da0cc7a2caa866f7500020f7
Karol Podlewski <karol.podlewski@hotmail.com>
2018-05-29 21:53:00 +0200

    New window

---
+++ src/guiLayer/AllEventsWindow.java
@@ -0,0 +1,109 @@
+package guiLayer;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import dataLayer.Event;
+import logicLayer.LogicLayer;
+import logicLayer.LogicLayerException;
+
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+import javax.swing.JTextField;
+import javax.swing.JList;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.TreeMap;
+
+import javax.swing.JScrollPane;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.ScrollPaneConstants;
+
+@SuppressWarnings("serial")
+public class AllEventsWindow extends JDialog
+{
+	private static AllEventsWindow dialog;
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void OpenWindow()
+	{
+		try
+		{
+			dialog = new AllEventsWindow();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setResizable(false);
+			dialog.setVisible(true);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	
+	/**
+	 * Create the dialog.
+	 */
+	public AllEventsWindow()
+	{		
+		setTitle("All Events");
+		setBounds(100, 100, 300, 350);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{0, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);	
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridwidth = 3;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		contentPanel.add(scrollPane, gbc_scrollPane);
+
+		TreeMap<Integer, Event> eventsList = Main.ll.getAllEvents();
+		
+		DefaultListModel dlm = new DefaultListModel();
+		
+		for (Event event : eventsList.values() )
+		{
+			System.out.println(event.toString());
+			dlm.addElement(event.toString());
+		}
+		
+		JList list = new JList(dlm);
+		scrollPane.setViewportView(list);
+
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER));
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
+		
+		JButton okButton = new JButton("OK");
+		okButton.setActionCommand("OK");
+		buttonPane.add(okButton);
+		getRootPane().setDefaultButton(okButton);
+		
+
+	}
+
+}
