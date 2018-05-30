c35180305a71157b428868efe751b4c0d6a34734
Karol Podlewski <karol.podlewski@hotmail.com>
2018-05-30 10:48:18 +0200

    Visual changes
    New radio button on reminders (not working),
    New calendar position

--- src/guiLayer/NewEventWindow.java
+++ src/guiLayer/NewEventWindow.java
@@ -7,34 +7,36 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridBagLayout;
+
 import javax.swing.JLabel;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 
-import logicLayer.LogicLayer;
+
 
 import javax.swing.SpinnerNumberModel;
 
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import javax.swing.JTextPane;
-import java.awt.TextArea;
+
 import javax.swing.JList;
 import javax.swing.JScrollPane;
-import java.awt.Color;
-import javax.swing.border.TitledBorder;
+
 
 import com.toedter.calendar.JCalendar;
 
-import javax.swing.ListModel;
+import dataLayer.Person;
+
 import javax.swing.ScrollPaneConstants;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+import javax.swing.ButtonGroup;
 
 @SuppressWarnings("serial")
 public class NewEventWindow extends JDialog
@@ -49,6 +51,7 @@ public class NewEventWindow extends JDialog
 	
 	private JSpinner spinner_1;
 	private JSpinner spinner_2;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 	
 	public static void OpenWindow()
 	{
@@ -67,15 +70,24 @@ public class NewEventWindow extends JDialog
 	
 	public NewEventWindow()
 	{	
+		addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {	MainWindow.OpenWindow();
+            }
+        });
+		
+		
 		setTitle("Add Event");
-		setBounds(100, 100, 576, 302);
+		setBounds(100, 100, 575, 326);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		{
 			jCalendar = new JCalendar();
-			jCalendar.setBounds(15, 13, 180, 159);
+			jCalendar.setBounds(22, 43, 180, 159);
 			contentPanel.add(jCalendar);
 		}
 		{
@@ -136,7 +148,7 @@ public class NewEventWindow extends JDialog
 			scrollPane.setBounds(303, 124, 235, 45);
 			contentPanel.add(scrollPane);
 			{
-				JList list = new JList();
+				JList<Person> list = new JList<Person>();
 				scrollPane.setViewportView(list);
 			}
 		}
@@ -155,12 +167,13 @@ public class NewEventWindow extends JDialog
 						{
 							calendar = jCalendar.getCalendar();							
 
-							calendar.set(Calendar.HOUR_OF_DAY, spinner_1.getComponentCount());
-							calendar.set(Calendar.MINUTE, spinner_2.getComponentCount());
+							calendar.set(Calendar.HOUR_OF_DAY, (Integer) spinner_1.getValue());
+							calendar.set(Calendar.MINUTE, (Integer) spinner_2.getValue());
 							
 							Main.ll.createEvent(textField.getText(), calendar, textField_1.getText(), textField_2.getText());
 							
 							dialog.dispose();
+							MainWindow.OpenWindow();
 						}
 						catch(Exception e)
 						{
@@ -176,31 +189,74 @@ public class NewEventWindow extends JDialog
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mousePressed(MouseEvent arg0) {
+						dialog.dispose();
+						MainWindow.OpenWindow();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 		{
 			JLabel label = new JLabel("Hour:");
-			label.setBounds(15, 181, 32, 16);
+			label.setBounds(22, 11, 32, 16);
 			contentPanel.add(label);
 		}
 		{
 			spinner_1 = new JSpinner();
 			spinner_1.setModel(new SpinnerNumberModel(12, 0, 23, 1));
-			spinner_1.setBounds(52, 178, 38, 22);
+			spinner_1.setBounds(59, 8, 38, 22);
 			contentPanel.add(spinner_1);
 		}
 		{
 			JLabel label = new JLabel("Minute:");
-			label.setBounds(113, 181, 43, 16);
+			label.setBounds(120, 11, 43, 16);
 			contentPanel.add(label);
 		}
 		{
 			spinner_2 = new JSpinner();
 			spinner_2.setModel(new SpinnerNumberModel(0, 0, 55, 5));
-			spinner_2.setBounds(157, 178, 38, 22);
+			spinner_2.setBounds(164, 8, 38, 22);
 			contentPanel.add(spinner_2);
 		}
+		
+		JLabel lblReminder = new JLabel("Reminder:");
+		lblReminder.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblReminder.setBounds(12, 213, 78, 16);
+		contentPanel.add(lblReminder);
+		
+		JRadioButton rdbtnNone = new JRadioButton("None");
+		rdbtnNone.setSelected(true);
+		buttonGroup.add(rdbtnNone);
+		rdbtnNone.setBounds(98, 209, 68, 25);
+		contentPanel.add(rdbtnNone);
+		
+		JRadioButton rdbtnMin = new JRadioButton("5 min");
+		buttonGroup.add(rdbtnMin);
+		rdbtnMin.setBounds(167, 209, 68, 25);
+		contentPanel.add(rdbtnMin);
+		
+		JRadioButton rdbtnMin_1 = new JRadioButton("30 min");
+		buttonGroup.add(rdbtnMin_1);
+		rdbtnMin_1.setBounds(239, 209, 78, 25);
+		contentPanel.add(rdbtnMin_1);
+		
+		JRadioButton rdbtnHour = new JRadioButton("1 hour");
+		buttonGroup.add(rdbtnHour);
+		rdbtnHour.setBounds(314, 209, 68, 25);
+		contentPanel.add(rdbtnHour);
+		
+		JRadioButton rdbtnHours = new JRadioButton("2 hours");
+		buttonGroup.add(rdbtnHours);
+		rdbtnHours.setBounds(386, 209, 78, 25);
+		contentPanel.add(rdbtnHours);
+		
+		JRadioButton rdbtnDay = new JRadioButton("1 day");
+		buttonGroup.add(rdbtnDay);
+		rdbtnDay.setBounds(464, 209, 59, 25);
+		contentPanel.add(rdbtnDay);
 	}
 }
