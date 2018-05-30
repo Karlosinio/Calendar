8859b8a04b012b7a5b14e3e1d57233267173d6d3
Karol Podlewski <karol.podlewski@hotmail.com>
2018-05-30 10:48:32 +0200

    Saving while exiting

--- src/guiLayer/MainWindow.java
+++ src/guiLayer/MainWindow.java
@@ -5,14 +5,11 @@ import com.toedter.calendar.JCalendar;
 import dataLayer.DataLayerException;
 import dataLayer.Event;
 import dataLayer.Person;
+import logicLayer.ExportException;
+import logicLayer.XMLSerializer;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.util.ArrayList;
-
 import javax.swing.border.EmptyBorder;
-
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -26,8 +23,13 @@ import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 
 @SuppressWarnings("serial")
@@ -42,19 +44,22 @@ public class MainWindow extends JFrame
 	private JLabel lblPlace;
 	private JLabel lblPeople;
 	private JLabel lblTime;
-	private JTextPane textPane;
-	private JTextPane textPane_1;
-	private JTextPane textPane_2;
-	private JTextPane textPane_3;
 	private JLabel lblReminder;
-	private JTextPane textPane_4;
+	private JTextPane tpName;
+	private JTextPane tpDescription;
+	private JTextPane tpPlace;
+	private JTextPane tpTime;
+	private JTextPane tpReminder;
 	private JLabel lblNewLabel;
 	private JScrollPane scrollPane;
-	private JList list;
+	private JList<Event> list;
 	private JScrollPane scrollPane_1;
 	private JList<Person> list_1;
 	private JButton btnAllEvents;
 
+	private static ArrayList<Event> eventsList;
+	private static DefaultListModel<Event> eventsDLM;
+	
 	/**
 	 * Launch the application.
 	 */	
@@ -78,12 +83,68 @@ public class MainWindow extends JFrame
 		});
 	}
 
+	
+	void dateSelection()
+	{
+    	eventsList = Main.ll.getAllEventsFromDate(jCalendar.getCalendar()); 
+		eventsDLM = new DefaultListModel<Event>();
+		
+		for(Event event : eventsList)
+			eventsDLM.addElement(event);
+		
+		list = new JList<Event>(eventsDLM);
+		list.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				eventSelection(list.getSelectedValue());
+			}
+		});
+		scrollPane.setViewportView(list);	
+	}
+	
+	void eventSelection(Event event)
+	{
+		tpName.setText(event.getName());
+		tpDescription.setText(event.getDescription());
+		tpPlace.setText(event.getPlace());
+		tpTime.setText(event.getTime());
+//		tpReminder.setText(event.getName());
+	}
+	
+	void clearEventFields()
+	{
+		tpName.setText("");
+		tpDescription.setText("");
+		tpPlace.setText("");
+		tpTime.setText("");		
+	}
+	
 	/**
 	 * Create the frame.
 	 * @throws DataLayerException 
 	 */
 	public MainWindow() throws DataLayerException
 	{
+		addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent we)
+            {	
+            	try
+				{
+					XMLSerializer.exportData("autosave.xml", Main.ll.getDataService() );
+				}
+            	catch (ExportException e)
+				{
+					ExceptionWindow.OpenWindow(e.getMessage());
+				}
+            	finally
+            	{
+            		frame.dispose();            		
+            	}
+            }
+        });		
+		
 		setTitle("Calendar");
 		setBounds(100, 100, 1201, 750);
 		contentPane = new JPanel();
@@ -94,97 +155,76 @@ public class MainWindow extends JFrame
 		jCalendar = new JCalendar();
 		jCalendar.setBounds(45, 43, 581, 375);
 		contentPane.add(jCalendar);	
-		
-		ArrayList<Event> eventsList = Main.ll.getAllEventsFromDate(jCalendar.getCalendar());
-		DefaultListModel<String> dlm = new DefaultListModel<String>();
-		
-		for(Event event : eventsList)
-		{
-			String string = new String();
-			string += event.getDate().getTime();
-			string += "  -  " + event.getName();
-			
-			dlm.addElement(string);
-		}
-		
+
 		lblNewLabel = new JLabel("Events list:");
 		lblNewLabel.setBounds(853, 43, 98, 25);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(lblNewLabel);
+		contentPane.add(lblNewLabel); 
+		
+		jCalendar.addPropertyChangeListener("calendar", new PropertyChangeListener()
+		{	@Override
+		    public void propertyChange(PropertyChangeEvent e)
+		    {	clearEventFields();
+				dateSelection();			}
+		});
 		
 		scrollPane = new JScrollPane();
+	
 		scrollPane.setBounds(701, 73, 402, 345);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		contentPane.add(scrollPane);
+
+
 		
-		list = new JList(dlm);
-		scrollPane.setViewportView(list);
+	//	eventSelection(list.getSelectedValue());
+		
+		dateSelection();
+
 		
 		lblName = new JLabel("Name:");
 		lblName.setBounds(95, 466, 38, 16);
 		contentPane.add(lblName);
 		
-		textPane = new JTextPane();
-		textPane.setBounds(138, 463, 258, 22);
-		contentPane.add(textPane);
+		tpName = new JTextPane();
+		tpName.setEditable(false);
+		tpName.setBounds(138, 463, 258, 22);
+		contentPane.add(tpName);
 		
 		lblTime = new JLabel("Time:");
 		lblTime.setBounds(441, 466, 34, 16);
 		contentPane.add(lblTime);
 		
-		textPane_3 = new JTextPane();
-		textPane_3.setBounds(480, 463, 146, 22);
-		contentPane.add(textPane_3);
+		tpTime = new JTextPane();
+		tpTime.setEditable(false);
+		tpTime.setBounds(480, 463, 146, 22);
+		contentPane.add(tpTime);
 		
 		lblReminder = new JLabel("Reminder:");
 		lblReminder.setBounds(713, 466, 60, 16);
 		contentPane.add(lblReminder);
 		
-		textPane_4 = new JTextPane();
-		textPane_4.setBounds(778, 463, 325, 22);
-		contentPane.add(textPane_4);
+		tpReminder = new JTextPane();
+		tpReminder.setBounds(778, 463, 325, 22);
+		contentPane.add(tpReminder);
 		
 		lblDescription = new JLabel("Description:");
 		lblDescription.setBounds(65, 534, 68, 16);
 		contentPane.add(lblDescription);
 		
-		textPane_1 = new JTextPane();
-		textPane_1.setBounds(138, 530, 488, 25);
-		contentPane.add(textPane_1);
-		
-		btnAllEvents = new JButton("All Events");
-		btnAllEvents.addMouseListener(new MouseAdapter()
-		{	@Override
-			public void mousePressed(MouseEvent e)
-			{
-				AllEventsWindow.OpenWindow();
-			}
-		});
-		btnAllEvents.setBounds(701, 530, 402, 25);
-		btnAllEvents.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		contentPane.add(btnAllEvents);
-		
+		tpDescription = new JTextPane();
+		tpDescription.setEditable(false);
+		tpDescription.setBounds(138, 530, 488, 25);
+		contentPane.add(tpDescription);
+	
 		lblPlace = new JLabel("Place:");
 		lblPlace.setBounds(98, 563, 35, 16);
 		contentPane.add(lblPlace);
 		
-		textPane_2 = new JTextPane();
-		textPane_2.setBounds(138, 560, 488, 22);
-		contentPane.add(textPane_2);
-		
-		btnNewButton = new JButton("Add Event");
-		btnNewButton.setBounds(701, 560, 402, 102);
-		btnNewButton.addMouseListener(new MouseAdapter()
-		{	@Override
-			public void mousePressed(MouseEvent arg0)
-			{
-				NewEventWindow.OpenWindow();
-			}
-		});
-		
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
-		contentPane.add(btnNewButton);
+		tpPlace = new JTextPane();
+		tpPlace.setEditable(false);
+		tpPlace.setBounds(138, 560, 488, 22);
+		contentPane.add(tpPlace);
 		
 		lblPeople = new JLabel("People:");
 		lblPeople.setBounds(90, 587, 43, 16);
@@ -198,11 +238,37 @@ public class MainWindow extends JFrame
 		
 		list_1 = new JList();
 		scrollPane_1.setViewportView(list_1);
-		DefaultListModel dml = new DefaultListModel();
+	//	DefaultListModel dml = new DefaultListModel();
 		
-		for (Event event : eventsList )
-		{
-			dml.addElement(event.toString());
-		}
+	//	for (Event event : eventsList )
+	//	{
+	//		dml.addElement(event.toString());
+	//	}
+		
+		btnAllEvents = new JButton("All Events");
+		btnAllEvents.addMouseListener(new MouseAdapter()
+		{	@Override
+			public void mousePressed(MouseEvent e)
+			{	AllEventsWindow.OpenWindow();	}
+		});
+		btnAllEvents.setBounds(701, 530, 402, 25);
+		btnAllEvents.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		contentPane.add(btnAllEvents);
+		
+		btnNewButton = new JButton("Add Event");
+		btnNewButton.setBounds(701, 560, 402, 102);
+		btnNewButton.addMouseListener(new MouseAdapter()
+		{	@Override
+			public void mousePressed(MouseEvent arg0)
+			{
+				NewEventWindow.OpenWindow();
+				frame.dispose();
+			}
+		});
+		
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 26));
+		contentPane.add(btnNewButton);
+		
+
 	}
 }
