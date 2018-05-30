c14f7419935d10210249c143e7d19bc645719ccb
Karol Podlewski <karol.podlewski@hotmail.com>
2018-05-30 10:46:22 +0200

    Reading XML file added

--- src/guiLayer/Main.java
+++ src/guiLayer/Main.java
@@ -1,15 +1,33 @@
 package guiLayer;
 
 import logicLayer.LogicLayer;
+import logicLayer.LogicLayerException;
+import logicLayer.XMLSerializer;
 
 public class Main
 {
 	public static LogicLayer ll;
 
+	
+	
+	
 	public static void main(String[] args)
 	{
 		ll = new LogicLayer();
-		MainWindow.OpenWindow();
+		
+		try
+		{
+			ll.setDataService(XMLSerializer.importData("autosave.xml"));
+		}
+		catch (LogicLayerException e)
+		{
+			ExceptionWindow.OpenWindow(e.getMessage());
+		}
+		finally
+		{			
+			MainWindow.OpenWindow();
+		}
+		
 	}
 
 }
